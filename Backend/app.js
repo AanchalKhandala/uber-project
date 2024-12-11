04f4d16c4fa102a34f27d11d2fadb1409fc58e46
Aanchal khandala <aanchalkhandala45@gmail.com>
2024-12-11 09:05:32 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+
+
+
+app.get('/',(req, res)=>{
+    res.send("hello from backend")
+})
+
+module.exports= app;
